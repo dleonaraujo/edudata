c0b9a4262cff3a230d5007f89ee752defd56e8dc
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2026-06-27 13:13:37 -0500

    fix: agregar comodin .vercel.app a ALLOWED_HOSTS para produccion

--- edudata/settings.py
+++ edudata/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
